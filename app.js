7fa65cb396042aed584496164250f50215bda3d7
kvl-ballester <kvl.ballester@gmail.com>
2025-03-06 11:35:14 +0100

    add cors middleware

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import express from 'express'
 import multer from "multer";
 import { uploadImageController, uploadMetadataController } from './controller/ipfsController.js'
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express()
-app.use(express.json());
+
+app.use(express.json())
+app.use(cors());
+
+
+
 
 const port = process.env.PORT || 3000
 
@@ -15,7 +21,6 @@ const storage = multer.memoryStorage(); // Guarda en RAM sin escribir en disco
 const upload = multer({ storage });
 
 app.post('/image', upload.single("image"), uploadImageController)
-
 app.post('/metadata', uploadMetadataController)
 
 app.listen(port, () => {
